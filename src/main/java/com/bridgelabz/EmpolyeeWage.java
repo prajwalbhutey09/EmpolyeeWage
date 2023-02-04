d1fff1053131f6ed921eb70ecd403e19e9cb4f61
Prajwalbhutey09 <prajwalbhutey09@gmail.com>
2023-02-04 00:49:20 -0800

    added:Code for empolyee attendence

--- src/main/java/com/bridgelabz/EmpolyeeWage.java
+++ src/main/java/com/bridgelabz/EmpolyeeWage.java
@@ -3,5 +3,10 @@ package com.bridgelabz;
 public class EmpolyeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        double check = Math.floor(Math.random() * 10) % 2;
+        if (check == 0) {
+            System.out.println("Employee is Present ");
+        } else
+            System.out.println("Employee is Absent ");
     }
 }

bda08e8b9f22c05daa0649236796a27f3337b58b
Prajwalbhutey09 <prajwalbhutey09@gmail.com>
2023-02-04 02:03:06 -0800

    added code for Daily Empolyee wage

--- src/main/java/com/bridgelabz/EmpolyeeWage.java
+++ src/main/java/com/bridgelabz/EmpolyeeWage.java
@@ -3,10 +3,16 @@ package com.bridgelabz;
 public class EmpolyeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        final int WAGE_PER_HR = 20;
+        final int FULL_DAY = 8;
+        int pay = 0;
+
         double check = Math.floor(Math.random() * 10) % 2;
         if (check == 0) {
-            System.out.println("Employee is Present ");
+            pay = WAGE_PER_HR * FULL_DAY;
+            System.out.println("Employee is Present and wage is "+pay);
         } else
             System.out.println("Employee is Absent ");
     }
+
 }
